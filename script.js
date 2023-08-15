a3f5a96a10f2fa5ee8c1751d73366c39c1c718cd
Oliver L <oliver.lkm@gmail.com>
2023-08-15 21:08:53 +1000

    Add logic to check for winner and display result

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const gameBoard = (() => {
   let state = [];
   const htmlElement = document.querySelector(".game-board");
+  const messageDisplayEl = document.querySelector(".message-display");
 
   const placeMarker = function (boardPosition, marker) {
     const boardIndex = boardPosition.dataset.boardIndex;
@@ -13,7 +14,15 @@ const gameBoard = (() => {
     boardPosition.classList.add("full");
   };
 
-  return { state, htmlElement, placeMarker };
+  const displayWinner = function (pattern, winner) {
+    winningPatterns[pattern].map((boardIndex) => {
+      htmlElement.children[boardIndex - 1].classList.add("win");
+      messageDisplayEl.textContent = `${winner.name} WINS! 🏆`;
+      messageDisplayEl.classList.remove("off");
+    });
+  };
+
+  return { state, htmlElement, placeMarker, displayWinner };
 })();
 
 // PLAYERS
@@ -35,9 +44,34 @@ const runGame = (function () {
     const boardPosition = e.target;
     if (boardPosition.textContent) return;
     activePlayer.play(boardPosition, activePlayer.marker);
-    console.log(gameBoard.state);
+    checkForWin(winningPatterns, gameBoard.state);
     activePlayer === player1
       ? (activePlayer = player2)
       : (activePlayer = player1);
   });
 })();
+
+const winningPatterns = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+const checkForWin = function (winningPatterns, state) {
+  for (let pattern = 0; pattern < winningPatterns.length; pattern++) {
+    const output = winningPatterns[pattern].map(
+      (boardIndex) => state[boardIndex - 1]
+    );
+    if (output.join("") === "XXX") endGame(pattern, player1);
+    if (output.join("") === "OOO") endGame(pattern, player2);
+  }
+};
+
+const endGame = function (pattern, winner) {
+  gameBoard.displayWinner(pattern, winner);
+};
